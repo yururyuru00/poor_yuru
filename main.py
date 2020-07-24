3cbba44285b9f30599a59f1d6bec444ef0e2d025
yuruOvO <30980083+yururyuru00@users.noreply.github.com>
2020-07-25 01:09:19 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from sample_class import C
+import func
+
+c1 = C('class a')
+c2 = C('class b')
+c3 = C('class c')
+cs = [c1, c2, c3]
+func.run(cs)
+
+
+# In[ ]:
+
+
+
+
