efb21f32063b5be5ab73ce592eb44f2cb2f47050
yuruOvO <30980083+yururyuru00@users.noreply.github.com>
2020-09-12 09:25:41 +0900

    Add files via upload

---
+++ geometric_model.py
@@ -0,0 +1,19 @@
+import torch
+import torch.nn.functional as F
+from torch_geometric.nn import GCNConv
+
+class Net(torch.nn.Module):
+    def __init__(self, dataset):
+        super(Net, self).__init__()
+        self.conv1 = GCNConv(dataset.num_features, 16)
+        self.conv2 = GCNConv(16, dataset.num_classes)
+
+    def forward(self, data):
+        x, edge_index = data.x, data.edge_index
+
+        x = self.conv1(x, edge_index)
+        x = F.relu(x)
+        x = F.dropout(x, training=self.training)
+        x = self.conv2(x, edge_index)
+
+        return F.log_softmax(x, dim=1)
