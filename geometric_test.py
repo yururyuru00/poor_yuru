11e3bc9f39dcf51c260cbaf7610cbc6f2b6bd0a8
yuruOvO <30980083+yururyuru00@users.noreply.github.com>
2020-09-13 09:34:23 +0900

    Add files via upload

--- geometric_test.py
+++ geometric_test.py
@@ -1,18 +1,31 @@
-from torch_geometric.datasets import CitationFull
-import torch.utils.data
+import torch
+import argparse
+from torch_geometric.datasets import Planetoid
+from utilities import ExtractSubstructureContextPair
 
-class DataLoader(torch.utils.data.DataLoader):
-    def __init__(self, dataset, batch_size=1, shuffle=True, **kwargs):
-        super(DataLoader, self).__init__(
-            dataset,
-            batch_size,
-            shuffle,
-            **kwargs)
+parser = argparse.ArgumentParser(
+    description='PyTorch implementation of pre-training of GNN')
+parser.add_argument('--l1', type=int, default=1,
+                    help='l1 (default: 1).')
+args = parser.parse_args()
 
-
-dataset = CitationFull(root='./data', name='Cora')
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+dataset = Planetoid(root='/tmp/Cora', name='Cora')
 print(dataset[0])
-loader = DataLoader(dataset, batch_size=1, shuffle=True)
 
-for step, batch in enumerate(loader):
-    print(batch)
+# model = Net(dataset).to(device)
+# optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=5e-4)
+
+'''model.train()
+for epoch in range(200):
+    optimizer.zero_grad()
+    out = model(data)
+    loss = F.nll_loss(out[data.train_mask], data.y[data.train_mask])
+    loss.backward()
+    optimizer.step()    
+
+model.eval()
+_, pred = model(data).max(dim=1)
+correct = float (pred[data.test_mask].eq(data.y[data.test_mask]).sum().item())
+acc = correct / data.test_mask.sum().item()
+print('Accuracy: {:.4f}'.format(acc))'''
