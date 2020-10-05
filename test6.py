b742cca72dca452f9695306a813a95bbaa30ab07
yuruOvO <jima971123@gmail.com>
2020-10-05 12:05:02 +0900

    origin master

--- test6.py
+++ test6.py
@@ -1,4 +1,12 @@
-import numpy as np
+from torch_geometric.datasets import KarateClub
+from torch_geometric import utils
 import torch
+import random
 
-print(torch.tensor([10, 0]))
+dataset = KarateClub()
+
+A = utils.to_dense_adj(dataset[0].edge_index)[0, :, :]
+adj = utils.dense_to_sparse(A)[0]
+
+print(dataset[0].edge_index)
+print(adj)
