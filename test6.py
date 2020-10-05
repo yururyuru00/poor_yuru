b97480c6a96c07e3d8c45dcc85c1ac561006bdd7
yuruOvO <jima971123@gmail.com>
2020-10-05 22:38:33 +0900

    note

--- test6.py
+++ test6.py
@@ -2,11 +2,10 @@ from torch_geometric.datasets import KarateClub
 from torch_geometric import utils
 import torch
 import random
+from torch_geometric.datasets import Planetoid
 
-dataset = KarateClub()
-
-A = utils.to_dense_adj(dataset[0].edge_index)[0, :, :]
-adj = utils.dense_to_sparse(A)[0]
-
-print(dataset[0].edge_index)
-print(adj)
+dataset = Planetoid(root='./data/experiment/', name='Cora')
+data = dataset[0]
+print(type(data.x))
+print(data.y)
+data.x = torch.nn.functional.one_hot(data.y, torch.max(data.y)+1).double()
