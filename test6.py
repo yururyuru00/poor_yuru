95acbcb37acdc80ae05d9e5d2ff13bf75da1474b
yuruOvO <jima971123@gmail.com>
2020-10-12 09:49:22 +0900

    note

--- test6.py
+++ test6.py
@@ -3,9 +3,17 @@ from torch_geometric import utils
 import torch
 import random
 from torch_geometric.datasets import Planetoid
+import itertools
+import numpy
 
 dataset = Planetoid(root='./data/experiment/', name='Cora')
 data = dataset[0]
-print(type(data.x))
-print(data.y)
-data.x = torch.nn.functional.one_hot(data.y, torch.max(data.y)+1).float()
+
+pair_list = set(itertools.combinations(range(5), 2))
+print(pair_list)
+pair_list_2 = set([(10, 10), (11, 11)])
+print(pair_list_2)
+pairs = list(pair_list) + list(pair_list_2)
+
+for (u, v) in pair_list_2:
+    print(u, v)
