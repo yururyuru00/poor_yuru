e66fa0684eb197bfbf821710d3a810bb058d9dbf
yururyuru00 <jima971123@gmail.com>
2020-11-07 13:39:00 +0900

    update

--- test6.py
+++ test6.py
@@ -1,8 +1,16 @@
 import torch
+import itertools
 
-t1 = torch.tensor([1, 2, 2])
-t2 = torch.tensor([1, 0, 2])
-t3 = torch.tensor([1, -1, 2])
+tensor1 = torch.randn(4)
+tensor2 = torch.randn(4,4)
+tensor3 = torch.randn(4)
 
+tensor1_ = torch.matmul(tensor1, tensor2)
+out1 = torch.matmul(tensor1_, tensor3)
+out2 = torch.FloatTensor(1)
 
-print(torch.mm(t1, t2))
+tensor = torch.FloatTensor([0])
+for i,j in itertools.combinations(range(3), 2):
+    ij = torch.FloatTensor([i+j])
+    tensor = torch.cat([tensor, ij], axis=0)
+print(tensor)
