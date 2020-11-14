1a6f1ba309f53a3c4846d9f7b872070116114513
yururyuru00 <jima971123@gmail.com>
2020-11-14 17:18:11 +0900

    note

--- test6.py
+++ test6.py
@@ -11,26 +11,20 @@ import itertools
 import matplotlib.pyplot as plt
 import networkx as nx
 
-from DeepGraphClustering import utilities
+from utilities_cp import GraphAugmenter
 
-
-n_class = 7
-dataset = 'Cora'
-step = 0
-edge = 0
 
 def graph_data_obj_to_nx(data):
 
     G = nx.Graph()
+    labels = data.y.cuda().cpu().detach().numpy().copy()
+    print(labels)
 
-    num_nodes = data.x.size()[0]
-    degree = [0 for w in range(num_nodes)]
-    for i,j in data.edge_index.T:
-            degree[i] += 1
-            degree[j] += 1
+    num_nodes = labels.shape[0]
     for idx in range(num_nodes):
         G.add_node(idx)
-    attr = {idx : {'degree' : degree_idx} for idx, degree_idx in enumerate(degree)}
+    attr = {idx : {'color' : color_list[label_id]} for idx, label_id 
+                in enumerate(labels)}
     nx.set_node_attributes(G, attr)
 
     edge_index = data.edge_index.cpu().numpy()
@@ -43,40 +37,35 @@ def graph_data_obj_to_nx(data):
     
     return G
 
+# parameters
+dataset = 'Cora'
+sample_label_idxes = [0, 2, 7] # label7 means augmented vertices
+color_list = ['#e41a1c', '#377eb8', '#4daf4a', '#984ea3', '#ff7f00', '#000000', '#a65628', '#ffff33']
+step = 1000
+edge_BA = 1
+edge_TF = 10
+
+# data loading
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 if(dataset == 'KarateClub'):
-    data = KarateClub(transform=utilities.GraphAugmenter(step,edge))
+    data = KarateClub(transform=GraphAugmenter(step, edge_BA, edge_TF))
 else:
     data = Planetoid(root='./data/experiment/', name=dataset,
-                        transform=utilities.GraphAugmenter(step,edge))
+                        transform=GraphAugmenter(step, edge_BA, edge_TF))
 data_augmented = data[0].to(device)
 
-# Graph visualization before augmentation
+# augmented graph visualization
 G = graph_data_obj_to_nx(data_augmented)
-pos = nx.spring_layout(G, k=0.9)
-node_size = [ d['degree']*10. for (n,d) in G.nodes(data=True)]
-nx.draw_networkx_nodes(G, pos, node_size=node_size)
-nx.draw_networkx_edges(G, pos, alpha=0.9, edge_color="c")
-plt.savefig('./Karate_step{}_edge{}'.format(step, edge))
+v_idxes_of_subgraph = []
+for v_id, v_label_id in enumerate(data_augmented.y):
+    if(v_label_id in sample_label_idxes):
+        v_idxes_of_subgraph.append(v_id)
+G_sub = nx.subgraph(G, nbunch=v_idxes_of_subgraph)
 
-print(data_augmented)
-
-# Graph visualization after augmentation
-
-
-'''def plot_G_contextG_pair(G, context_G, center_substract_idx, c_i):
-
-    nodes_context_G = [n_i for n_i in context_G.nodes]
-    color_map = []
-    for node in G:
-        if node == center_substract_idx:
-            color_map.append('red')
-        elif node in nodes_context_G:
-            color_map.append('blue')
-        else:  # when node is in a substract graph G
-            color_map.append('orange')
-
-    plt.subplot(111)
-    nx.draw(G, node_color=color_map,
-            with_labels=True)
-    plt.savefig('./data/experiment/test/G_context_G_pair_c{}.png'.format(c_i))'''
+plt.figure(figsize = (20, 20))
+v_color_list = nx.get_node_attributes(G_sub, 'color').values()
+pos = nx.spring_layout(G_sub, seed=42)
+nx.draw_networkx_nodes(G_sub, pos, node_size=25, node_color=v_color_list)
+nx.draw_networkx_edges(G_sub, pos, width=0.70)
+plt.savefig('./{}_step{}_BA{}_TF{}'.format(dataset, step, edge_BA, edge_TF), 
+            dpi = 150, bbox_inches="tight")
